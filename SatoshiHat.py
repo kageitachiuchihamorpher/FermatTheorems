74cecd49794a02d2bd9699b09d60d907f9903bcd
kageitachiuchihamorpher <kageitachisharingan@gmail.com>
2026-02-05 02:00:47 +0000

    complete Satoshi Hat Quantum Gravity code

--- SatoshiHat.py
+++ SatoshiHat.py
@@ -1,3 +1,7 @@
 #The following variables are to serve as global variables.
-x = 5; y = 1; z = 1000;
+x = 5; y = 1; z = 1000; i = (1/2); c = 3 * ((10)^8); N = ((x^2 + y^2 + z^2)^((x^2 + y^2 + z^2) -1));
 
+def SatoshiHat(i):
+    for i in range(4):
+        print(str("# of Quantum Gravity Calculations: ") + str(int(N * (i^5) * c)) + str("Newtons"));
+SatoshiHat(i);
